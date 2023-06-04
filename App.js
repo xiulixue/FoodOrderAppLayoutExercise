4c34c974998fb6de9992f7b1e1df680b17e747d0
Xue <xue@Macbooks-MacBook-Pro.local>
2023-06-05 01:53:09 +1000

    front end mobile app

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import RootNavigation from './src/RootNavigation';
+
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <RootNavigation />
   );
 }
 
